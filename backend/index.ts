b103fd53182e21bd014340987113f299097ee138
DJ-InfinityCoder <nitiandilip13@gmail.com>
2025-11-08 20:03:05 +0530

    dj commit

--- backend/index.ts
+++ backend/index.ts
@@ -1,11 +1,11 @@
 import { VercelRequest, VercelResponse } from '@vercel/node';
 import app from './src/infrastructure/server/expressApp';
 
-export default async (req: VercelRequest, res: VercelResponse) => {
-  // Run the Express app handler here
-  return new Promise((resolve, reject) => {
-    app(req, res); // Call Express handler
-    res.on('finish', () => resolve()); // Ensure to resolve after response is finished
-    res.on('error', reject);  // Reject if there is an error
+export default async (req: VercelRequest, res: VercelResponse): Promise<void> => {
+  // Handle the request in the express app
+  return new Promise<void>((resolve, reject) => {
+    app(req, res); // Delegate the request to Express
+    res.on('finish', () => resolve());  // Resolve when the response is sent
+    res.on('error', (err) => reject(err));  // Reject on error
   });
 };
